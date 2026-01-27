b803d318d1d49f0867e0ea857abc0d24339a81b3
Ilua <semin574@yandex.ru>
2026-01-27 18:44:03 +0300

    Add pr1.py

---
+++ pr1.py
@@ -0,0 +1,9 @@
+# Практическая работа №1
+
+surname_name = input("Ваши фамилия, имя? ")
+age = input("Сколько Вам лет? ")
+city = input("Где вы живете? ")
+
+print("\nВаши фамилия, имя:", surname_name)
+print("Ваш возраст:", age)
+print("Вы живете в", city)
